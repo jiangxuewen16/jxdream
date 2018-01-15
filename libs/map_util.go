e157da5d4d26a40f33c19a5c294b2ab02d85c50e
jiangxuewen <445251692@qq.com>
2018-01-15 18:18:08 +0800

    map_util

--- libs/map_util.go
+++ libs/map_util.go
@@ -1,17 +1,31 @@
 package libs
 
+import (
+	"reflect"
+	"fmt"
+)
+
 //import "reflect"
 
 //todo:这里通过反射来做
-func Map2Struct(maps map[interface{}]interface{}, obj interface{}) (interface{}, error) {
-	/*t := reflect.TypeOf(obj)
-	v := reflect.ValueOf(obj)
+func Map2Struct(maps map[interface{}]interface{}, obj interface{}) {
+	t := reflect.TypeOf(obj)
 	mutable := reflect.ValueOf(&obj).Elem()
+	fmt.Println(mutable)
+	mutable.FieldByName(t.Field(1).Name).CanSet()
 
 	for i := 0 ; i < t.NumField(); i++ {
-		if value,ok := maps[t.Field(i)]; ok {
-			type := t.Field(i).Type
-			mutable.FieldByName(t.Field(i)).SetString(value)
+		if value,ok := maps[t.Field(i).Name]; ok {
+			if mutable.FieldByName(t.Field(i).Name).CanSet() {
+				switch t.Field(i).Type.String() {
+				case "int":
+					mutable.FieldByName(t.Field(i).Name).SetInt(value.(int64))
+				case "string":
+					mutable.FieldByName(t.Field(i).Name).SetString(value.(string))
+				default:
+
+				}
+			}
 		}
-	}*/
+	}
 }
