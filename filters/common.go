b80bb26da8d307db435e187bee05d442fe1bad37
DESKTOP-094KS21\jiang <445251692@qq.com>
2018-03-10 11:58:50 +0800

    修复get请求的bug

--- filters/common.go
+++ filters/common.go
@@ -4,7 +4,6 @@ import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
 	"log"
-	"regexp"
 	"jxdream/common"
 	"encoding/json"
 	"jxdream/libs"
@@ -19,7 +18,6 @@ var HasLogin = func(ctx *context.Context) {
 	log.Println("requset uri :", ctx.Request.RequestURI)
 	log.Println("request data:", string(ctx.Input.RequestBody))
 	requestBody := ctx.Input.RequestBody
-	isLogin := false		//默认未登陆
 
 	//如果没有请求参数，构建一个默认请求参数
 	if string(requestBody) != "" {
@@ -28,7 +26,7 @@ var HasLogin = func(ctx *context.Context) {
 		jwtClaims, err :=libs.GetClaims(requestParam.Header.JWT)
 		if err != nil {
 			jwtClaims.IsLogin = false
-			requestParam, err := common.BuildRequest(jwtClaims,nil, "token 校验失败", common.TOKEN_VALIDATION_FAILED)
+			requestParam, err := common.BuildRequest(jwtClaims,nil, err.Error(), common.TOKEN_VALIDATION_FAILED)
 			libs.CheckError(err)
 
 			requestByte,err := json.Marshal(requestParam)
@@ -36,13 +34,20 @@ var HasLogin = func(ctx *context.Context) {
 
 			ctx.Input.RequestBody = requestByte
 		}
-		isLogin = jwtClaims.IsLogin
+		//isLogin = jwtClaims.IsLogin
+	} else {
+		requestParam,_ := common.BuildDefaultRequest(common.REQUEST_NOT_HAS_HEADER)
+		requestParamStr,err := json.Marshal(requestParam)
+		if err != nil {
+			libs.CheckError(err)
+		}
+		ctx.Input.RequestBody = requestParamStr
 	}
 
-	log.Println("whether login :", isLogin)
+	//log.Println("whether login :", isLogin)
 
 	//登录页面
-	loginUrl := beego.AppConfig.String("LoginUrl")
+	/*loginUrl := beego.AppConfig.String("LoginUrl")
 	match, _ := regexp.MatchString("^" + loginUrl, ctx.Request.RequestURI)
 	if (!isLogin && !match) || string(requestBody) == "" {
 		requestParam,_ := common.BuildDefaultRequest(common.REQUEST_NOT_HAS_HEADER)
@@ -51,6 +56,6 @@ var HasLogin = func(ctx *context.Context) {
 			libs.CheckError(err)
 		}
 		ctx.Input.RequestBody = requestParamStr
-	}
+	}*/
 }
 
